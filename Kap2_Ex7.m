6277a2a543461030d270efc548327f54e495b63a
unknown <n.vinzenz@googlemail.com>
2017-10-25 13:20:18 +0200

    Ex 2.7 a)

--- Kap2_Ex7.m
+++ Kap2_Ex7.m
@@ -1,5 +1,18 @@
 clc;
 format long;
-#Ex 2.7
-printf ("***Ex 2.7\n");
-#TODO
+#Ex 2.7 a)
+printf ("***Ex 2.7 a)\n");
+
+#Number of cleaning iterations
+# Poison concentration: (a*v) / ((a * v) + ((b - a) * v))
+# Poisen concentration short -> a / b
+
+function [cleaningIteration] = calcCleaningIterations(a, b, e)
+  poisCon = 1;
+  cleaningIteration = 0;
+  while poisCon > e
+    poisCon = (a / b) ** cleaningIteration;
+    cleaningIteration++;
+  end;
+endfunction
+calcCleaningIterations(0.01, 1, 10**-9)
