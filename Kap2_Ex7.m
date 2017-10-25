bc01e6caced12873e6f1745f94733959e5f3b41c
Nico Vinzenz <n.vinzenz@googlemail.com>
2017-10-25 20:53:08 +0200

    Ex7 c) + Arbeitsumgebung

--- Kap2_Ex7.m
+++ Kap2_Ex7.m
@@ -1,5 +1,11 @@
 clc;
-format long;
+format short;
+#Ex 2.7 
+##GLOBAL VARIABLES
+a = 0.01
+b = 0.01
+e = 10 ** -9
+
 #Ex 2.7 a)
 printf ("***Ex 2.7 a)\n");
 
@@ -10,9 +16,30 @@ printf ("***Ex 2.7 a)\n");
 function [cleaningIteration] = calcCleaningIterations(a, b, e)
   poisCon = 1;
   cleaningIteration = 0;
+  if b <= a
+    error('Error: b must be bigger than a!');
+  end;
   while poisCon > e
     poisCon = (a / b) ** cleaningIteration;
     cleaningIteration++;
   end;
 endfunction
-calcCleaningIterations(0.01, 1, 10**-9)
+cleanIterationSum = calcCleaningIterations(a, b, e)
+
+#Ex 2.7 b)
+printf ("***Ex 2.7 b)\n");
+
+function [waterVolume] = calcRequiredWaterVolume(a, b, iterations)
+  waterVolume = (b - a) * iterations;
+endfunction
+
+requiredWaterVolume = calcRequiredWaterVolume(a, b, cleanIterationSum)
+
+#Ex 2.7 c)
+#TODO
+
+#Ex 2.7 d)
+#TODO
+
+#Ex 2.7 r)
+#TODO
