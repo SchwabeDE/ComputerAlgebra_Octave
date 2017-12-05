02ad69ec194b806c1a893706c23c4353e9599e79
Nico Vinzenz <n.vinzenz@googlemail.com>
2017-12-05 03:10:06 +0100

    Kap5 Ex10 d)

--- Kapitel5/Kap5_Ex10.m
+++ Kapitel5/Kap5_Ex10.m
@@ -18,7 +18,30 @@ end;
 
 #endresult = squareRoot(1342,4,20)
 
-endresult = squareRoot(rand(1)*1000,rand(1)*10,20)
+#endresult = squareRoot(rand(1)*1000,rand(1)*10,20)
+
+#plot(endresult, "r");
+
+
+#Ex 5.10 d)
+disp("***Ex 5.10 d)\n");
+
+function [x] = squareRootConv(a, xInit, iterations)
+  if a <= 0 || xInit <= 0
+    error("Parameters must be positive!");
+  end;
+  x = [xInit];
+  for i = 1 : iterations,
+    xNew = x(end) / a;
+    x = [x xNew];
+  end;
+end;
+
+#a = rand(1)*1000
+#xInit = rand(1)*10
+
+a = 5
+xInit = 5
+endresult = squareRootConv(a,xInit,20)
 
 plot(endresult, "r");
-
