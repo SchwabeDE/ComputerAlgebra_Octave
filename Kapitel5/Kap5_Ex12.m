0d0e171c96fd143437f139b06a8410bc80b54a92
Nico Vinzenz <n.vinzenz@googlemail.com>
2017-12-08 01:09:32 +0100

    Ex 5.12 Interval bisection method angepasst

--- Kapitel5/Kap5_Ex12.m
+++ Kapitel5/Kap5_Ex12.m
@@ -1,9 +1,11 @@
 source("../opt.m");
+graphics_toolkit gnuplot;
 disp("\n*********************************************************************");
 #Ex 5.12
 disp("***Ex 5.12\n");
 
-function [currPoint] = intervalBisection(f, aInit, bInit, threshold)
+
+function [m] = intervalBisection(f, aInit, bInit, threshold)
   a = aInit;
   b = bInit;
   
@@ -15,29 +17,47 @@ function [currPoint] = intervalBisection(f, aInit, bInit, threshold)
     m = (1/2) * (a + b);
     currPoint = f(m)
     
-    if(f(m) < 0)
+    if(currPoint < 0)
       a = m;
-    elseif(f(m) > 0)
+    elseif(currPoint > 0)
       b = m;
     else
-      disp("Root found!")
+      printf("Root found after %d Iterations!\n", rounds);
     end
-  until(f(m) == 0 || threshold <= rounds )
+  until(currPoint == 0 || threshold <= rounds )
 endfunction
 
+#{
 function [ret] = f(x)
   if(x == 1 || x == -1)
     x = 1;
   end
   ret = (x*(1-x)) / (1- x**2);
 endfunction
-  
-  
-result = intervalBisection(@f, -4, -1/2, 150)
+#}
 
-xResults = [];
-for x = -50 : 10
-  xResults = [xResults f(x)];
+function [ret] = f(x)
+  ret = x - 2;
+endfunction
+
+  
+a = -4;
+b = 4;
+result = intervalBisection(@f, a, b, 150)
+
+xPlot = [];
+yPlot = [];
+for x = - 10 : 1 : 10
+  xPlot = [xPlot x];
+  yPlot = [yPlot f(x)];
 end
 
-#plot(xResults)
+xPlot
+yPlot
+
+plot(xPlot, yPlot);
+grid on;
+grid minor on;
+
+refresh;
+refreshdata;
