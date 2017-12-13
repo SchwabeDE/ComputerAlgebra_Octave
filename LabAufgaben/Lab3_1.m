69bb9906a80a5f08848e30e703e8fbd08ef9bf61
Nico Vinzenz <n.vinzenz@googlemail.com>
2017-12-13 22:48:41 +0100

    Lab 3.1 c) d)

--- LabAufgaben/Lab3_1.m
+++ LabAufgaben/Lab3_1.m
@@ -20,3 +20,39 @@ end;
 number = 15
 result1 = iterativeFac(number)
 result2 = recursiveFac(number)
+
+#{
+##TODO
+#Lab Ex 3.1 b)
+disp("***Lab Ex 3.1 a)\n");
+
+function [y] = f(z)
+  y = (log(1/z))**(z-1);
+end;
+
+result3 = quad(@f, 0, 1)
+#}
+
+#Lab Ex 3.1 c)
+disp("***Lab Ex 3.1 c)\n");
+
+function [ret] = fib(n)
+  if n > 1
+    ret = fib(n-1) + fib(n-2);
+  elseif (n = 0) || (n = 1)
+    ret = 1;
+  else
+    ret = 0;
+  end;
+end;
+
+computingTime = []
+tic()
+for n = 1 : 20
+  fib(n)
+  computingTime = [computingTime toc];
+end;
+
+
+computingTime
+plot(computingTime)
