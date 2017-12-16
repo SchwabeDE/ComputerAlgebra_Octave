082a9e28888736fca41aea82283845772176f4fc
Nico Vinzenz <n.vinzenz@googlemail.com>
2017-12-16 15:48:28 +0100

    Lab 3.2 b)

--- LabAufgaben/Lab3_2.m
+++ LabAufgaben/Lab3_2.m
@@ -34,10 +34,40 @@ for x = -2 * pi : 0.1 : 2 * pi
   xStepsize = [xStepsize x];
 end;
 
+#{
 close all;
-
 plot(xStepsize, taylorResult, 'r');
 hold on;
 plot(xStepsize, sinResult, 'b');
 axis([-2 * pi, 2 * pi, -2, 2], "normal");
 hold off;
+#}
+
+disp("\n*********************************************************************");
+#Lab Ex 3.2 b)
+disp("***Lab Ex 3.2 b)\n");
+clear;
+
+#IMPORTANT: 
+#This function is only for the e**x function correct, because its derivations 
+#are all the same and the function is monotonically strictly increasing.
+function [y, r] = Maclaurin(x, f, degree)
+  y = 0;
+  for n = 0 : degree
+    y += (f(0) / factorial(n)) * (x ** n);
+  end;
+  r = (f(0) / factorial(degree+1)) * (x ** degree + 1);
+end;
+
+f = @(x) e**x;
+
+x = 1
+for n = 1 : 20
+  n
+  [approxE, maclaurinRemainder] = Maclaurin(x, f, n)
+end;
+#n = 13 and x = 1 gives a precision of 10 digits
+realE = e**x
+
+
+
