37a907293c00fc8dd605935f1a4255286a03e94d
Nico Vinzenz <n.vinzenz@googlemail.com>
2017-12-30 13:26:02 +0100

    mod ex6.8

--- Kapitel6/Kap6_Ex8.m
+++ Kapitel6/Kap6_Ex8.m
@@ -11,8 +11,17 @@ function [x] = tridiagonalAlgorithm(A, d)
   c = diag(A, 1)
 
   n = rows(b)
-  #x = zeros(n)(:,1)
+  
+  #check if diagonally dominant
+  for i = 1 : n
+    if(issquare(A) == 0)
+      error("MATRIX IS NOT SQUARE");
+    elseif( abs(A(i,i)) <= (sum(abs(A(i,:))) - abs(A(i,i))))
+      error("MATRIX IS NOT DIAGONALLY DOMINANT!");
+    end;
+  end;
 
+  #Elimination
   for k = 2 : n
     m = c(k-1) / b(k-1);
     b(k) = b(k) - m * c(k-1);
@@ -20,6 +29,7 @@ function [x] = tridiagonalAlgorithm(A, d)
   end;
 
   x(n) = d(n) / b(n);  #Addition to the skript algorithm
+  #Backward substitution
   for k = n - 1 : -1 : 1
     d(n) = d(n) / b(n);
     d(k) = (d(k) - c(k)*d(k+1)) / b(k);
@@ -37,6 +47,9 @@ d = [-12; 12; 12]
 
 #A = [4]
 #d = [12]
+
+#A = [4 5 0; 5 4 5; 0 5 4] #NOT DIAGONALLY DOMINANT
+#d = [-12; 12; 12]
 
 tridiagSol = tridiagonalAlgorithm(A, d)
 
