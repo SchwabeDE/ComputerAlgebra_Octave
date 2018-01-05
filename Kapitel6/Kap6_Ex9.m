29db3b87982c37b4d91366dada7cdedde3e06737
Nico Vinzenz <n.vinzenz@googlemail.com>
2018-01-05 23:28:10 +0100

    Teste Werte aus ex6.11 in ex6.9

--- Kapitel6/Kap6_Ex9.m
+++ Kapitel6/Kap6_Ex9.m
@@ -117,7 +117,48 @@ y = f(x)
 valueTable_xy = [x' y']
 [a, b, c, d, yListInterpol] = naturalSplineInterpolation(valueTable_xy, xListForInterpol)
 
+#{
 plot(xListForInterpol, yListInterpol)
 hold on;
 plot(valueTable_xy(:,1), valueTable_xy(:,2))
 hold off;
+#}
+
+disp("\n*********************************************************************");
+#Ex 6.11 Test
+disp("***Ex 6.11 Test\n");
+
+function [x, y] = IFS(n, a, b, c, d)
+  x = [1];
+  y = [1];
+  for i = 1 : n 
+    x(end+1) = a * y(end) + b;
+    y(end+1) = c * x(end) + d;
+  end;
+end;
+
+n = 20;
+[x, y] = IFS(n, 0.9, -0,9, -0.9, 0.9)
+
+a = min(x);
+b = max(x);
+stepSize = abs(a-b) / (n-1);
+x_f = [a:stepSize:b]
+
+y_f = interp1(x, y, x_f, "spline")
+
+
+valueTable_xy = [x' y']
+[a, b, c, d, yListInterpol] = naturalSplineInterpolation(valueTable_xy, x_f)
+
+
+plot(x, y, "k--")
+hold on;
+plot(x_f, y_f, "c")
+plot(x_f, yListInterpol, "b")
+hold off;
+
+
+
+
+
