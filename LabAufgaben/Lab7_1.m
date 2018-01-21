8b7971d696f9cb2a46c4a59e1988ac027297d167
Nico Vinzenz <nv-171738@hs-weingarten.de>
2018-01-21 04:46:00 +0100

    Lab7.1 a)

--- LabAufgaben/Lab7_1.m
+++ LabAufgaben/Lab7_1.m
@@ -4,28 +4,32 @@ disp("\n*********************************************************************");
 #Ex 7.1 a)
 disp("***Ex 7.1 a)\n");
 
-f_unitCircle = @(x,y) x.**2 .+ y.**2
+#f_unitCircle = @(x,y) x.**2 .+ y.**2
+
+f_unitCircle = @(x) sqrt(1.-x.**2)
 
 function [area] = naiveMonteCarloUnitCircle(f_unitCircle, randX, randY, B, H)
-  f_values = f_unitCircle(randX, randY);
+  f_values = f_unitCircle(randX);
+
   
-  numberHitsUnderCurve = sum(f_values < 1)
-  numberHitsInsideRectangle = length(f_values)
+  numberHitsUnderCurve = sum(f_values >= randY);
+  numberHitsInsideRectangle = length(f_values);
   
   area = numberHitsUnderCurve / numberHitsInsideRectangle * B * H;
 end;
 
 randNumbers = 50000000;
+#randNumbers = 10;
 randX = rand(randNumbers,1);
 randY = rand(randNumbers,1);
 
 B = 2
 H = 2
 
-naiveMonteCarloUnitCircle(f_unitCircle, randX, randY, B, H)
+nMCUC = naiveMonteCarloUnitCircle(f_unitCircle, randX, randY, B, H)
 
-function [area] = meanMonteCarloUnitCircle(f_unitCircle, randX, randY, a, b)
-  f_values = f_unitCircle(randX, randY);
+function [area] = meanMonteCarloUnitCircle(f_unitCircle, randX, a, b)
+  f_values = f_unitCircle(randX);
   
   area = ( (b - a) / length(f_values) ) * sum(f_values);
 end;
@@ -35,4 +39,4 @@ b = 1
 
 grid = [-1 : 0.0001: 1];
 
-meanMonteCarloUnitCircle(f_unitCircle, grid, grid, a, b)
+mMCUC = meanMonteCarloUnitCircle(f_unitCircle, randX, a, b) * 2
